ab07eb44d9f95ce0750a057c29e2cfe18c109f4b
Davi de Melo <davimelovasc@gmail.com>
2018-04-19 01:56:30 -0300

    Autenticacao de adm ao tentar logar no backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
 
+  root 'site/home#index'
+
+  #Site routes
   get '/index' => 'site/home#index'
+
+  #BackOffice routes
+  get '/admin' => 'backoffice/dashboard#index'
 
   namespace :backoffice do
     get '/' => 'dashboard#index'
   end
 
+
+
   devise_for :admins
   devise_for :members
-
-  root 'site/home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
